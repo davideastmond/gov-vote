43e9874f78bc11cad7392e18e306c6b37a6d795e
David Eastmond <davebuck1981@gmail.com>
2026-02-17 13:28:06 -0500

    refactor: remove unused database imports and contestGroups retrieval from admin dashboard load function

--- src/routes/admin/dashboard/+page.server.ts
+++ src/routes/admin/dashboard/+page.server.ts
@@ -1,5 +1,3 @@
-import { db } from '$lib/server/db';
-import { contestGroup } from '$lib/server/db/schema';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
@@ -8,15 +6,7 @@ export const load: PageServerLoad = async (event) => {
 		return redirect(302, '/admin/login');
 	}
 
-	const contestGroups = await db
-		.select({
-			contestGroupId: contestGroup.id,
-			contestGroupTitle: contestGroup.title,
-			contestGroupDescription: contestGroup.description
-		})
-		.from(contestGroup);
 	return {
-		session,
-		contestGroups: contestGroups
+		session
 	};
 };
