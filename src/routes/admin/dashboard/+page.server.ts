53d692c27e51a61896b53419ce272cd451d9b1c8
David Eastmond <davebuck1981@gmail.com>
2026-02-17 08:04:31 -0500

    feat: load contest groups in admin dashboard

--- src/routes/admin/dashboard/+page.server.ts
+++ src/routes/admin/dashboard/+page.server.ts
@@ -1,3 +1,5 @@
+import { db } from '$lib/server/db';
+import { contestGroup } from '$lib/server/db/schema';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
@@ -6,7 +8,15 @@ export const load: PageServerLoad = async (event) => {
 		return redirect(302, '/admin/login');
 	}
 
+	const contestGroups = await db
+		.select({
+			contestGroupId: contestGroup.id,
+			contestGroupTitle: contestGroup.title,
+			contestGroupDescription: contestGroup.description
+		})
+		.from(contestGroup);
 	return {
-		session
+		session,
+		contestGroups: contestGroups
 	};
 };
