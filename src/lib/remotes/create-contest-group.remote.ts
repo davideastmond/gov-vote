7de729012cd0ed84bcaef9018e4549b1b4b1d979
David Eastmond <davebuck1981@gmail.com>
2026-02-16 20:30:24 -0500

    feat: enhance createContestGroup command to handle contests, admins, and polling stations

--- src/lib/remotes/create-contest-group.remote.ts
+++ src/lib/remotes/create-contest-group.remote.ts
@@ -1,8 +1,16 @@
 import { command } from '$app/server';
 import type { PollingStationAddress } from '$lib/definitions/address';
-import type { Contest } from '$lib/definitions/contest-group';
+import type { Contest, ContestItemType } from '$lib/definitions/contest-group';
 import { db } from '$lib/server/db';
-import { contestGroup } from '$lib/server/db/schema';
+import {
+	address,
+	adminContestGroup,
+	contest,
+	contestGroup,
+	contestGroupPollingStation,
+	contestItem,
+	pollingStation
+} from '$lib/server/db/schema';
 import { createContestGroupValidator } from '$lib/validators/create-contest-group.validator';
 import z from 'zod';
 
@@ -16,7 +24,9 @@ type InputData = {
 };
 export const createContestGroup = command(
 	'unchecked',
-	async (data: InputData): Promise<{ errors: Record<string, string> } | void> => {
+	async (
+		data: InputData
+	): Promise<{ errors: Record<string, string> } | { contestGroupId: string } | void> => {
 		// Validate the incoming data using the createContestGroupValidator
 		try {
 			createContestGroupValidator.parse(data);
@@ -34,7 +44,7 @@ export const createContestGroup = command(
 		}
 
 		const { id, title, description, adminIds, contests, pollingStationAddresses } = data;
-
+		console.log('Creating contest group with data:', data);
 		// Create the contest group.
 		try {
 			const newContestGroup = await db
@@ -46,9 +56,100 @@ export const createContestGroup = command(
 				})
 				.returning({ id: contestGroup.id });
 
-			// Associate the admins with the contest group.
+			await writeContestDataToDb(newContestGroup[0].id, contests);
+			// Associate the admins with the contest group via the join table
+			if (adminIds && adminIds.length > 0) {
+				await writeAdminContestsDataToDb(newContestGroup[0].id, adminIds);
+			}
+
+			await writePollingStationDataToDb(newContestGroup[0].id, pollingStationAddresses);
+			return { contestGroupId: newContestGroup[0].id };
 		} catch (error) {
 			console.error(error);
 		}
 	}
 );
+
+async function writeContestDataToDb(contestGroupId: string, contests: Contest[]) {
+	// Create contests and contest items - they are nested
+	const extractedContestsData: {
+		id: string;
+		contestGroupId: string;
+		title: string;
+		description?: string;
+	}[] = [];
+	const extractedContestItemsData: {
+		id: string;
+		contestId: string;
+		title: string;
+		auxiliaryText: string;
+		contestItemType: ContestItemType;
+	}[] = [];
+
+	contests.forEach((contest) => {
+		extractedContestsData.push({
+			id: contest.id,
+			contestGroupId: contestGroupId,
+			title: contest.title,
+			description: contest.description
+		});
+
+		contest.items.forEach((item) => {
+			extractedContestItemsData.push({
+				id: item.id,
+				contestId: contest.id,
+				title: item.title,
+				auxiliaryText: item.auxiliaryText,
+				contestItemType: item.contestItemType
+			});
+		});
+	});
+
+	await db.insert(contest).values(extractedContestsData);
+	await db.insert(contestItem).values(extractedContestItemsData);
+}
+
+async function writeAdminContestsDataToDb(contestGroupId: string, adminIds: string[]) {
+	const mappedAdminContestGroupData = adminIds.map((adminId) => {
+		return {
+			id: crypto.randomUUID(), // For the database primary key in the join table
+			adminId: adminId,
+			contestGroupId: contestGroupId
+		};
+	});
+	await db.insert(adminContestGroup).values(mappedAdminContestGroupData);
+}
+
+async function writePollingStationDataToDb(
+	contestGroupId: string,
+	pollingStationAddresses: PollingStationAddress[]
+) {
+	// TBD: Implement this function to write polling station data to the database and associate it with the contest group
+	/* - address, pollingStation, contestGroupPollingStation */
+	// 1. Write the address data.
+
+	for await (const a of pollingStationAddresses) {
+		const addressId = crypto.randomUUID();
+		await db.insert(address).values({
+			id: addressId,
+			streetAddress: a.streetAddress,
+			city: a.city,
+			state: a.state,
+			zipCode: a.zipCode
+		});
+		// 2. Write the polling station data, associating it with the address
+		const pollingStationId = crypto.randomUUID();
+		await db.insert(pollingStation).values({
+			id: pollingStationId,
+			name: a.name,
+			addressId: addressId
+		});
+		// 3. Associate the polling station with the contest group via the join table
+		await db.insert(contestGroupPollingStation).values({
+			id: crypto.randomUUID(),
+			contestGroupId: contestGroupId,
+			pollingStationId: pollingStationId
+		});
+		console.log('Inserted polling station with ID:', pollingStationId);
+	}
+}
