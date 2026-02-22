ba514f4d7a1796ca820f3c7647f6150769742c00
David Eastmond <davebuck1981@gmail.com>
2026-02-22 09:26:38 -0500

    feat: implement ballot normalization and add related types and tests

---
+++ src/routes/voter/ballot/+page.server.ts
@@ -0,0 +1,58 @@
+import { db } from '$lib/server/db';
+import {
+	contest,
+	contestGroup,
+	contestItem,
+	user,
+	voterCard,
+	voterEligibility
+} from '$lib/server/db/schema';
+import { verifyJWT } from '$lib/server/utils/jwt/jwt';
+import { redirect } from '@sveltejs/kit';
+import { and, eq, or } from 'drizzle-orm';
+import type { PageServerLoad } from './$types';
+import { normalizeBallotRows } from './ballot-transform';
+
+export const load: PageServerLoad = async (event) => {
+	// We should be able to get cookies and jwt data from here
+	const voterToken = event.cookies.get('voter_token');
+
+	// If the voterToken is undefined, redirect to the voter/start page
+	if (!voterToken) {
+		return redirect(302, '/voter/start');
+	}
+
+	// Get the voterCodeCode from the JWT
+	const { sub } = await verifyJWT<{ sub: string }>(voterToken);
+
+	// If the sub is not defined, redirect to the voter/start page
+	if (!sub) {
+		return redirect(302, '/voter/start');
+	}
+	const ballotData = await db
+		.select()
+		.from(voterCard)
+		.where(
+			and(
+				eq(voterCard.cardCode, sub),
+				or(eq(voterCard.cardStatus, 'active'), eq(voterCard.cardStatus, 'generated'))
+			)
+		)
+		.innerJoin(contestGroup, eq(contestGroup.id, voterCard.contestGroupId))
+		.innerJoin(
+			voterEligibility,
+			and(
+				eq(voterEligibility.contestGroupId, contestGroup.id),
+				eq(voterEligibility.isEligible, true)
+			)
+		)
+		.innerJoin(user, eq(user.id, voterEligibility.userId))
+		.innerJoin(contest, eq(contest.id, voterEligibility.contestId))
+		.innerJoin(contestItem, eq(contestItem.contestId, contest.id));
+
+	const contests = normalizeBallotRows(ballotData);
+
+	return {
+		contests
+	};
+};
