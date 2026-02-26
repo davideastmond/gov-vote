94572eb054cfcf4df9b51326f62da1d3d1d1830d
David Eastmond <davebuck1981@gmail.com>
2026-02-25 22:27:38 -0500

    feat: enhance contest group loading logic to include user role checks and streamline data retrieval

--- src/routes/admin/dashboard/contest-groups/view/[contestGroupId]/+page.server.ts
+++ src/routes/admin/dashboard/contest-groups/view/[contestGroupId]/+page.server.ts
@@ -1,6 +1,7 @@
 import { db } from '$lib/server/db';
 import {
 	address,
+	adminContestGroup,
 	contest,
 	contestGroup,
 	contestGroupPollingStation,
@@ -12,30 +13,17 @@ import type { PageServerLoad } from './$types';
 
 // Loads a specific contest group by ID, along with its associated contests and polling stations, for display on the contest group details page in the admin dashboard.
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
 	const { contestGroupId } = params;
 
+	const session = await locals.auth();
+
 	// Contest group details - title and description and polling locations
-	const contestGroupData = await db
-		.select({
-			contestGroupId: contestGroup.id,
-			contestGroupTitle: contestGroup.title,
-			contestGroupDescription: contestGroup.description,
-			pollingStationId: pollingStation.id,
-			pollingStationName: pollingStation.name,
-			pollingStationStreetAddress: address.streetAddress,
-			pollingStationCity: address.city,
-			pollingStationState: address.state,
-			pollingStationZipCode: address.zipCode
-		})
-		.from(contestGroup)
-		.where(eq(contestGroup.id, contestGroupId))
-		.leftJoin(
-			contestGroupPollingStation,
-			eq(contestGroupPollingStation.contestGroupId, contestGroup.id)
-		)
-		.leftJoin(pollingStation, eq(pollingStation.id, contestGroupPollingStation.pollingStationId))
-		.leftJoin(address, eq(address.id, pollingStation.addressId));
+	// If user is super_admin, they can view any contest group. If user is admin, they can only view contest groups they are associated with. Otherwise, return 403.
+	const contestGroupData = await retrieveContestGroup(
+		session?.user.role as 'admin' | 'super_admin',
+		session?.user.id
+	);
 
 	const ballotContests = await db
 		.select({
@@ -56,4 +44,53 @@ export const load: PageServerLoad = async ({ params }) => {
 		ballotContests: ballotContests,
 		contestGroupId: contestGroupId
 	};
+
+	async function retrieveContestGroup(accessLevel?: 'admin' | 'super_admin', userId?: string) {
+		if (accessLevel === 'super_admin') {
+			return db
+				.select({
+					contestGroupId: contestGroup.id,
+					contestGroupTitle: contestGroup.title,
+					contestGroupDescription: contestGroup.description,
+					pollingStationId: pollingStation.id,
+					pollingStationName: pollingStation.name,
+					pollingStationStreetAddress: address.streetAddress,
+					pollingStationCity: address.city,
+					pollingStationState: address.state,
+					pollingStationZipCode: address.zipCode
+				})
+				.from(contestGroup)
+				.where(eq(contestGroup.id, contestGroupId))
+				.leftJoin(
+					contestGroupPollingStation,
+					eq(contestGroupPollingStation.contestGroupId, contestGroup.id)
+				)
+				.leftJoin(
+					pollingStation,
+					eq(pollingStation.id, contestGroupPollingStation.pollingStationId)
+				)
+				.leftJoin(address, eq(address.id, pollingStation.addressId));
+		}
+		return db
+			.select({
+				contestGroupId: contestGroup.id,
+				contestGroupTitle: contestGroup.title,
+				contestGroupDescription: contestGroup.description,
+				pollingStationId: pollingStation.id,
+				pollingStationName: pollingStation.name,
+				pollingStationStreetAddress: address.streetAddress,
+				pollingStationCity: address.city,
+				pollingStationState: address.state,
+				pollingStationZipCode: address.zipCode
+			})
+			.from(contestGroup)
+			.where(eq(contestGroup.id, contestGroupId))
+			.leftJoin(adminContestGroup, eq(adminContestGroup.adminId, userId as string))
+			.leftJoin(
+				contestGroupPollingStation,
+				eq(contestGroupPollingStation.contestGroupId, contestGroup.id)
+			)
+			.leftJoin(pollingStation, eq(pollingStation.id, contestGroupPollingStation.pollingStationId))
+			.leftJoin(address, eq(address.id, pollingStation.addressId));
+	}
 };
